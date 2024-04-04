66b36cd68ba11a062dcd2f2e7fdbc6bef6daaaa0
n-namnt <nguyent.nam.97@gmail.com>
2024-04-05 00:24:21 +0700

    up code

--- UnityHub/assets/js/main.js
+++ UnityHub/assets/js/main.js
@@ -1,17 +1,6 @@
-/**
-* Template Name: TheEvent
-* Template URL: https://bootstrapmade.com/theevent-conference-event-bootstrap-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function () {
     "use strict";
 
-    /**
-     * Easy selector helper function
-     */
     const select = (el, all = false) => {
         el = el.trim()
         if (all) {
@@ -21,30 +10,18 @@
         }
     }
 
-    /**
-     * Easy event listener function
-     */
     const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all)
-        if (selectEl) {
-            if (all) {
-                selectEl.forEach(e => e.addEventListener(type, listener))
-            } else {
-                selectEl.addEventListener(type, listener)
-            }
+        if (all) {
+            select(el, all).forEach(e => e.addEventListener(type, listener))
+        } else {
+            select(el, all).addEventListener(type, listener)
         }
     }
 
-    /**
-     * Easy on scroll event listener 
-     */
     const onscroll = (el, listener) => {
         el.addEventListener('scroll', listener)
     }
 
-    /**
-     * Navbar links active state on scroll
-     */
     let navbarlinks = select('#navbar .scrollto', true)
     const navbarlinksActive = () => {
         let position = window.scrollY + 200
@@ -62,15 +39,12 @@
     window.addEventListener('load', navbarlinksActive)
     onscroll(document, navbarlinksActive)
 
-    /**
-     * Scrolls to an element with header offset
-     */
     const scrollto = (el) => {
         let header = select('#header')
         let offset = header.offsetHeight
 
         if (!header.classList.contains('header-scrolled')) {
-            offset -= 20
+            offset -= 10
         }
 
         let elementPos = select(el).offsetTop
@@ -80,9 +54,6 @@
         })
     }
 
-    /**
-     * Toggle .header-scrolled class to #header when page is scrolled
-     */
     let selectHeader = select('#header')
     if (selectHeader) {
         const headerScrolled = () => {
@@ -96,9 +67,6 @@
         onscroll(document, headerScrolled)
     }
 
-    /**
-     * Back to top button
-     */
     let backtotop = select('.back-to-top')
     if (backtotop) {
         const toggleBacktotop = () => {
@@ -112,18 +80,12 @@
         onscroll(document, toggleBacktotop)
     }
 
-    /**
-     * Mobile nav toggle
-     */
     on('click', '.mobile-nav-toggle', function (e) {
         select('#navbar').classList.toggle('navbar-mobile')
         this.classList.toggle('bi-list')
         this.classList.toggle('bi-x')
     })
 
-    /**
-     * Mobile nav dropdowns activate
-     */
     on('click', '.navbar .dropdown > a', function (e) {
         if (select('#navbar').classList.contains('navbar-mobile')) {
             e.preventDefault()
@@ -131,9 +93,6 @@
         }
     }, true)
 
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
     on('click', '.scrollto', function (e) {
         if (select(this.hash)) {
             e.preventDefault()
@@ -149,9 +108,6 @@
         }
     }, true)
 
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
     window.addEventListener('load', () => {
         if (window.location.hash) {
             if (select(window.location.hash)) {
@@ -160,14 +116,85 @@
         }
     });
 
-
-    /**
-     * Gallery Slider
-     */
-    new Swiper('.gallery-slider', {
+    new Swiper('.clients-slider', {
         speed: 400,
         loop: true,
-        centeredSlides: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 40
+            },
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 60
+            },
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 80
+            },
+            992: {
+                slidesPerView: 6,
+                spaceBetween: 120
+            }
+        }
+    });
+
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-item',
+                layoutMode: 'fitRows'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function (e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function (el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+                aos_init();
+            }, true);
+        }
+
+    });
+
+    const portfolioLightbox = GLightbox({
+        selector: '.portfokio-lightbox'
+    });
+
+    new Swiper('.portfolio-details-slider', {
+        speed: 400,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
+    new Swiper('.testimonials-slider', {
+        speed: 600,
+        loop: true,
         autoplay: {
             delay: 5000,
             disableOnInteraction: false
@@ -181,41 +208,27 @@
         breakpoints: {
             320: {
                 slidesPerView: 1,
-                spaceBetween: 20
+                spaceBetween: 40
             },
-            575: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            768: {
+
+            1200: {
                 slidesPerView: 3,
-                spaceBetween: 20
-            },
-            992: {
-                slidesPerView: 5,
-                spaceBetween: 20
             }
         }
     });
 
-
-    /**
-     * Buy tickets select the ticket type on click
-     */
-    on('show.bs.modal', '#buy-ticket-modal', function (event) {
-        select('#buy-ticket-modal #ticket-type').value = event.relatedTarget.getAttribute('data-ticket-type')
-    })
-
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
+    function aos_init() {
         AOS.init({
             duration: 1000,
-            easing: 'ease-in-out',
+            easing: "ease-in-out",
             once: true,
             mirror: false
-        })
+        });
+    }
+    window.addEventListener('load', () => {
+        aos_init();
     });
 
-})()
+    new PureCounter();
+
+})();
